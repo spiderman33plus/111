d9e91f30de7c8aa4bc8e76dec848a9a84fa830a6
DiVic11 <vmsotoca@gmail.com>
2026-04-09 17:11:26 +0200

    Generate 1000 text files with results

--- 111_PRO/111.py
+++ 111_PRO/111.py
@@ -1,1 +1,14 @@
+a = "111"
+b = a * 1000
+c = b * 1000
+d = c * 1000
+nombre_base = "111"
+veces = 1000
+resultado = d
+for i in range(veces):
+    nombre_archivo = f"{nombre_base}_{i+1}.txt"
+    
+    with open(nombre_archivo, "w") as f:
+        f.write("Resultado: " + str(resultado))
 
+print("Archivos creados")
